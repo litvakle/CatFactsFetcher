2f6eeaf3400d07e99ce55ad74aa52e354c4b0eb4
Litvak Lev <sn.delui@gmail.com>
2022-08-18 10:58:45 +0500

    Fetch delivers error on client error

--- CatFactsFetcher/CatFactsNinjaFetcher.swift
+++ CatFactsFetcher/CatFactsNinjaFetcher.swift
@@ -8,19 +8,27 @@
 import Foundation
 
 public protocol HTTPClient {
-    func fetch(from url: URL)
+    typealias Result = Swift.Result<Data, Error>
+    
+    func fetch(from url: URL, _ completion: @escaping (Result) -> Void)
 }
 
 public final class CatFactsNinjaFetcher {
     let client: HTTPClient
     let url: URL
     
+    public typealias Result = Swift.Result<Data, Error>
+    
     public init(client: HTTPClient, url: URL) {
         self.client = client
         self.url = url
     }
     
-    public func fetch() {
-        client.fetch(from: url)
+    public func fetch(_ completion: @escaping((Result) -> Void)) {
+        client.fetch(from: url) { result in
+            if case let .failure(error) = result {
+                completion(.failure(error))
+            }
+        }
     }
 }
--- CatFactsFetcherTests/CatFactsFetcherTests.swift
+++ CatFactsFetcherTests/CatFactsFetcherTests.swift
@@ -21,16 +21,45 @@ class CatFactsFetcherTests: XCTestCase {
         let url = URL(string: "http://any-url.com")!
         let sut = CatFactsNinjaFetcher(client: client, url: url)
         
-        sut.fetch()
+        sut.fetch() { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_fetch_deliversErrorOnClientError() {
+        let client = HTTPClientSpy()
+        let url = URL(string: "http://any-url.com")!
+        let sut = CatFactsNinjaFetcher(client: client, url: url)
+        
+        let expectedError = NSError(domain: "any error", code: 1)
+        let exp = expectation(description: "Waiting for fetch completion")
+        sut.fetch { result in
+            switch result {
+            case .success:
+                XCTFail("Expected filure, received \(result) instead)")
+            case let .failure(error as NSError):
+                XCTAssertEqual(error.code, expectedError.code)
+                XCTAssertEqual(error.domain, expectedError.domain)
+            }
+            exp.fulfill()
+        }
+        
+        client.complete(withError: expectedError)
+        
+        wait(for: [exp], timeout: 1.0)
     }
 }
 
 private class HTTPClientSpy: HTTPClient {
     var requestedURLs = [URL]()
+    var completions = [(HTTPClient.Result) -> Void]()
     
-    func fetch(from url: URL) {
+    func fetch(from url: URL, _ completion: @escaping (HTTPClient.Result) -> Void) {
         requestedURLs.append(url)
+        completions.append(completion)
+    }
+    
+    func complete(withError error: Error, at index: Int = 0) {
+        completions[index](.failure(error))
     }
 }
